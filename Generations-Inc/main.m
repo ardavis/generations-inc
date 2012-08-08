b64a73f5959584a3665391d229596396fac973d6
Andrew Davis <andrew@andrewrdavis.net>
2012-08-08 17:12:46 -0400

    Initial Commit

---
+++ Generations-Inc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Generations-Inc
+//
+//  Created by Andrew Davis on 8/8/12.
+//  Copyright (c) 2012 Enginerds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
