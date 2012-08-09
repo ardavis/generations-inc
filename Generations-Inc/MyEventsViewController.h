62f8a50fb78c49280b54482dc26f7160e2520574
Andrew Davis <andrew@andrewrdavis.net>
2012-08-08 20:08:28 -0400

    Main Menu

---
+++ Generations-Inc/MyEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyEventsViewController.h
+//  Generations-Inc
+//
+//  Created by Andrew Davis on 8/8/12.
+//  Copyright (c) 2012 Enginerds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyEventsViewController : UIViewController
+
+@end
---
+++ Generations-Inc/MyEventsViewController.m
@@ -0,0 +1,43 @@
+//
+//  MyEventsViewController.m
+//  Generations-Inc
+//
+//  Created by Andrew Davis on 8/8/12.
+//  Copyright (c) 2012 Enginerds. All rights reserved.
+//
+
+#import "MyEventsViewController.h"
+
+@interface MyEventsViewController ()
+
+@end
+
+@implementation MyEventsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
